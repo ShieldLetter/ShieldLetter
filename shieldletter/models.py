5c8b01fa42ef059bd75b146d2127b9f51f31b554
pja9740807! <p.jinapark@gmail.com>
2023-12-27 20:11:17 +0900

    index 수정, urls 수정, views 수정

--- shieldletter/models.py
+++ shieldletter/models.py
@@ -6,11 +6,15 @@ class User(models.Model):
     username = models.CharField(max_length=50)
     phone_num = models.CharField(max_length=20)
     email = models.EmailField(max_length=100)
+    def __str__(self):
+        return self.username
 
 class Admin(models.Model):
     admin_id = models.CharField(max_length=30, primary_key=True)
     admin_pw = models.CharField(max_length=100)
     admin_name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.admin_name
 
 class Board(models.Model):
     id = models.AutoField(primary_key=True)
@@ -21,3 +25,6 @@ class Board(models.Model):
     file = models.BinaryField(null=True)
     date = models.DateTimeField(auto_now_add=True)
     category = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.title
