7acf5d7d880979db7b79c44a8a7f276a6691d3dc
js-u-u <vjsdy5@gmail.com>
2023-12-28 00:24:03 +0900

    게시글 클릭 시 세부페이지로 이동

--- shieldletter/urls.py
+++ shieldletter/urls.py
@@ -8,9 +8,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login, name="login"),
     path("signup", views.signup, name="signup"),
-    path("board", views.board, name="board"),
-    
-    # URL:80/blog/숫자로 접속하면 게시글-세부페이지(posting)
-    path('blog/<int:pk>',views.board, name="board"),
+    path('board/<int:id>',views.board, name="board"),
     path("board_write", views.board_write, name="board_write")
 ]
--- shieldletter/views.py
+++ shieldletter/views.py
@@ -4,7 +4,7 @@ from .models import *
 def index(request):
     # 모든 board를 가져와 postlist에 저장합니다
     boardlist = Board.objects.all()
-    # blog.html 페이지를 열 때, 모든 Post인 postlist도 같이 가져옵니다 
+    # index.html 페이지를 열 때, 모든 Post인 postlist도 같이 가져옵니다 
     return render(request, 'index.html', {'boardlist':boardlist})
 
 def login(request):
@@ -13,8 +13,11 @@ def login(request):
 def signup(request):
     return render(request, 'signup.html')
 
-def board(request):
-    return render(request, 'board.html')
+def board(request, id):
+    # 게시글(Post) 중 id를 이용해 하나의 게시글(post)을 검색
+    post = Post.objects.get(id=id)
+    # board.html 페이지를 열 때, 찾아낸 게시글(post)을 post라는 이름으로 가져온다.
+    return render(request, 'board.html', {'post':post})
 
 def board_write(request):
     return render(request, 'board_write.html')
