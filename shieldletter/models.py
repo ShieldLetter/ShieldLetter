e82ab5a734492dbab0305c7165cb74c6eda059b4
js-u-u <vjsdy5@gmail.com>
2023-12-28 13:35:19 +0900

    일반 user 로그인 가능

--- shieldletter/models.py
+++ shieldletter/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-class User(models.Model):
-    user_id = models.CharField(max_length=30, primary_key=True)
-    user_pw = models.CharField(max_length=100)
-    username = models.CharField(max_length=50)
-    phone_num = models.CharField(max_length=20)
-    email = models.EmailField(max_length=100)
-    def __str__(self):
-        return self.username
+# class User(models.Model):
+#     user_id = models.CharField(max_length=30, primary_key=True)
+#     user_pw = models.CharField(max_length=100)
+#     username = models.CharField(max_length=50)
+#     phone_num = models.CharField(max_length=20)
+#     email = models.EmailField(max_length=100)
+#     def __str__(self):
+#         return self.username
 
 class Admin(models.Model):
     admin_id = models.CharField(max_length=30, primary_key=True)
--- shieldletter/views.py
+++ shieldletter/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 
