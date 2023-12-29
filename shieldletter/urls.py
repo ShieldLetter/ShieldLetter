1a68ccb8d142b2a7057d242fead0bd7f4d6a39c0
pja9740807! <p.jinapark@gmail.com>
2023-12-30 02:30:29 +0900

    static 디렉토리 수정, 템플릿 파일들 수정 등

--- shieldletter/urls.py
+++ shieldletter/urls.py
@@ -1,16 +1,16 @@
-from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path
-from .views import signup
+from django.views.decorators.cache import never_cache
+
 
 appname="shieldletter"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", never_cache(views.index), name="index"),
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path("signup/", views.signup, name="signup"),
@@ -19,7 +19,6 @@ urlpatterns = [
     path("board/<int:id>/update", views.board_update, name="board_update"),
     path("board/<int:id>/delete", views.board_delete, name="board_delete"),
 ]
-
 
 # 이미지 URL설정
 urlpatterns += static(settings.UPLOAD_URL, document_root=settings.UPLOAD_ROOT)
---
+++ static/js/board_write.js
@@ -0,0 +1,23 @@
+(() => {
+    function validateForm() {
+        var title = document.getElementById('title').value.trim();
+        var content = document.getElementById('content').value.trim();
+        var fileInput = document.getElementById('file');
+        var filePath = fileInput.value;
+        var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+        var submitButton = document.getElementById('submit');
+
+        if (!title || !content || !allowedExtensions.exec(filePath)) {
+            submitButton.disabled = true;
+            return false;
+        } else {
+            submitButton.disabled = false;
+            return true;
+        }
+        console.log("filePath=> "+filePath);
+    }
+
+    document.getElementById('title').addEventListener('input', validateForm);
+    document.getElementById('content').addEventListener('input', validateForm);
+    document.getElementById('file').addEventListener('change', validateForm);
+})();
