90dfa2fc14d79bf781853da1cd8004b91d3480f8
pja9740807! <p.jinapark@gmail.com>
2023-12-30 22:26:10 +0900

    주석 추가

--- static/js/board_write.js
+++ static/js/board_write.js
@@ -1,21 +1,22 @@
 (() => {
+    // form 유효성 검사
     function validateForm() {
-        var title = document.getElementById('title').value.trim();
-        var content = document.getElementById('content').value.trim();
+        var title = document.getElementById('title').value.trim(); // title에서 공백 제거
+        var content = document.getElementById('content').value.trim(); // content에서 공백 제거
         var fileInput = document.getElementById('file');
         var filePath = fileInput.value;
-        var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+        var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i; // 허용되는 파일 확장자 정의
         var submitButton = document.getElementById('submit');
-
+        // 제목이나 내용이 없거나 허용되지 않는 파일 확장자인 경우
         if (!title || !content || !allowedExtensions.exec(filePath)) {
-            submitButton.disabled = true;
+            submitButton.disabled = true; // 전송 버튼 비활성화
             return false;
         } else {
-            submitButton.disabled = false;
+            submitButton.disabled = false; // 전송 버튼 활성화
             return true;
         }
     }
-
+    // title, content, file 부분에 입력이나 변경 발생할 때마다 유효성 검사 실행
     document.getElementById('title').addEventListener('input', validateForm);
     document.getElementById('content').addEventListener('input', validateForm);
     document.getElementById('file').addEventListener('change', validateForm);
